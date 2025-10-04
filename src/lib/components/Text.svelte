a324edc581086b786bf5a3b217cc1c5bbf2d5aba
reversesingularity <cmodina70@gmail.com>
2025-10-05 01:29:58 +1300

    Fix text contrast - improve readability in Chrome/Safari/Opera

--- src/lib/components/Text.svelte
+++ src/lib/components/Text.svelte
@@ -22,7 +22,7 @@
       case 'code':
         return 'rounded-sm border border-zinc-950/10 bg-zinc-950/2.5 px-0.5 text-sm font-medium text-zinc-950 sm:text-[0.8125rem] dark:border-white/20 dark:bg-white/5 dark:text-white';
       default:
-        return 'text-base/6 text-zinc-500 sm:text-sm/6 dark:text-zinc-400';
+        return 'text-base/6 text-zinc-700 sm:text-sm/6 dark:text-zinc-300';
     }
   });
 
